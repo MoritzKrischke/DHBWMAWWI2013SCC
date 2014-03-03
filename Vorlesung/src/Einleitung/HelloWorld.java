7cc1292d075df598c0e950ed6fcfd8710f330f35
Matthias Berg <matthiasuweberg@googlemail.com>
2014-03-03 13:34:50 +0100

    Start des Projektes für die Vorlesung WWI2013 SCC

---
+++ Vorlesung/src/Einleitung/HelloWorld.java
@@ -0,0 +1,16 @@
+// Erstes Java Programm
+
+package Einleitung;
+
+// Unsere erste Klasse
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		
+		// Ausgabe eines Textes auf der Konsole
+		System.out.println("Hallo Klaus!!");
+
+	}
+
+}
