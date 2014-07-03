65c8381d3e26bcaad861a0dc971d3ce871b5f3f5
Matthias Uwe Berg <matthias.berg@sap.com>
2014-07-03 17:07:18 +0200

    Einwohnermeldeamt - Erweiterte Aufgabe

---
+++ Vorlesung/src/Beispiele/EinwohnerMeldeamt/Extended/Run.java
@@ -0,0 +1,13 @@
+package Beispiele.EinwohnerMeldeamt.Extended;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		// init UI
+		new MeldeamtUI("Einwohnermeldeamt");
+
+
+	}
+
+}
